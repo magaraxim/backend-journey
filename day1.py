20daef025c32867c18b5723569c1f3f8ee9ad1e4
Ashim Magar <ashimbuddha48@gmail.com>
2026-06-16 16:51:36 +0545

    Day 1 - Python basics, variables, calculator, about me

---
+++ day1.py
@@ -0,0 +1,90 @@
+# Exercise 1 - Hello World
+# My very first Python Program
+
+print("Hello, World!")
+print("My name is Ashim Magar")
+print("I am learning Python")
+print("I will be backend developer")
+print("Day 1 starts NOW!")
+
+# Exercise 2 - Variables
+
+name = "Ashim Magar"
+age = 19
+city = "Baglung"
+goal = "Backend Developer"
+months = 6
+
+print("name:", name)
+print("Age:", age)
+print("City:", city)
+print("Goal:", goal)
+print("months to job:", months)
+
+# Exercise 3 - User Input
+
+name = input("What's your name? ")
+city = input("Where are you from? ")
+
+print("Hello,", name)
+print("You are from", city)
+print("Welcome to coding")
+
+# Exercise 4 - Numbers
+
+num1 = 20
+num2 = 8
+
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+# Exercise 5 -> F-string
+
+name = "Ashim Magar"
+age = 19
+goal = "Backend Developer"
+months = 6
+
+print(f"My name is {name}")
+print(f"I am {age} years old.")
+print(f"My goal is {goal}")
+print(f"I will get job in {months} months")
+
+# Exercise 6 - Calculator
+
+print("-> MY CALCULATOR <-")
+
+num1 = int(input("Enter a first number: "))
+num2 = int(input("Enter a second number: "))
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} x {num2} = {num1 * num2}")
+print("---------------------------------")
+
+# Exercise 7 - About Me
+# My first complete program
+
+print("===============================")
+print("           ABOUT ME            ")
+print("===============================")
+
+name = input("What's your name? ")
+age = input("What's yor age? ")
+city = input("Where are you from? ")
+goal = input("What's your goal? ")
+
+print("===============================")
+print(f" Name = {name}")
+print(f" Age = {age}")
+print(f" City = {city}")
+print(f" Goal = {goal}")
+
+print("===============================")
+print(f"Hello, {name}!")
+print("You will achieve your goal!")
+print("Keep coding evey single day!")
+print("===============================")
